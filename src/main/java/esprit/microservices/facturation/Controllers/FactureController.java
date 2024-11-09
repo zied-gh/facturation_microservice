eef391b4f625f53785bb06988d4202196d541d64
zied ghanem <ziedghanem44@gmail.com>
2024-11-09 18:27:38 +0100

    projet facturation1

--- src/main/java/esprit/microservices/facturation/Controllers/FactureController.java
+++ src/main/java/esprit/microservices/facturation/Controllers/FactureController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/factures")
+@RequestMapping("/factures")
 public class FactureController {
 
     @Autowired
--- src/main/java/esprit/microservices/facturation/Controllers/PaiementController.java
+++ src/main/java/esprit/microservices/facturation/Controllers/PaiementController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/paiements")
+@RequestMapping("/paiements")
 public class PaiementController {
 
     @Autowired
